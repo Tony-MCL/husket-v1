22daefc4481098be524d926f1536e11ab1b9d893
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-02-17 13:51:39 +0100

    Update SettingsDrawer.tsx

--- src/components/SettingsDrawer.tsx
+++ src/components/SettingsDrawer.tsx
@@ -7,6 +7,7 @@ import { useUiStore } from "../state/uiStore";
 export function SettingsDrawer() {
   const open = useUiStore((s) => s.settingsOpen);
   const close = useUiStore((s) => s.closeSettings);
+  const openTrash = useUiStore((s) => s.openTrash);
 
   if (!open) return null;
 
@@ -38,7 +39,21 @@ export function SettingsDrawer() {
 
         <div className="hr" />
 
-        <div className="smallHelp">Papirkurv kommer i neste pakke (global administrasjon).</div>
+        <button
+          className="flatBtn"
+          onClick={() => {
+            close();
+            openTrash();
+          }}
+        >
+          Papirkurv
+        </button>
+
+        <div className="hr" />
+
+        <div className="smallHelp">
+          GPS-toggle, rating packs og full Viewer kommer i neste pakker.
+        </div>
       </aside>
     </>
   );
