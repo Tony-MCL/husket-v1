adf56c693fcbbb8da1910d176ffb878a5e61b7f3
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-02-17 13:50:38 +0100

    Update uiStore.ts

--- src/state/uiStore.ts
+++ src/state/uiStore.ts
@@ -4,11 +4,14 @@
 // ===============================
 import { create } from "zustand";
 import type { AlbumFilters, LifeId, PanelId } from "../domain/types";
-import { loadActiveLifeId, loadPanelHintCount, saveActiveLifeId, savePanelHintCount } from "../data/local";
+import {
+  loadActiveLifeId,
+  loadPanelHintCount,
+  saveActiveLifeId,
+  savePanelHintCount
+} from "../data/local";
 
-type ViewerState =
-  | { isOpen: false }
-  | { isOpen: true; husketId: string };
+type ViewerState = { isOpen: false } | { isOpen: true; husketId: string };
 
 type UiState = {
   // global ui
@@ -17,6 +20,9 @@ type UiState = {
   viewer: ViewerState;
   settingsOpen: boolean;
   albumFilters: AlbumFilters;
+
+  // global admin overlays (still "global", not tied to life)
+  trashOpen: boolean;
 
   // onboarding
   panelHintCount: number;
@@ -30,6 +36,9 @@ type UiState = {
 
   openSettings: () => void;
   closeSettings: () => void;
+
+  openTrash: () => void;
+  closeTrash: () => void;
 
   setAlbumFilters: (patch: Partial<AlbumFilters>) => void;
   clearAlbumFilters: () => void;
@@ -46,6 +55,8 @@ export const useUiStore = create<UiState>((set, get) => ({
   settingsOpen: false,
   albumFilters: { ...EMPTY_FILTERS },
 
+  trashOpen: false,
+
   panelHintCount: loadPanelHintCount(),
 
   setActiveLifeId: (lifeId) => {
@@ -60,6 +71,9 @@ export const useUiStore = create<UiState>((set, get) => ({
 
   openSettings: () => set({ settingsOpen: true }),
   closeSettings: () => set({ settingsOpen: false }),
+
+  openTrash: () => set({ trashOpen: true }),
+  closeTrash: () => set({ trashOpen: false }),
 
   setAlbumFilters: (patch) => set({ albumFilters: { ...get().albumFilters, ...patch } }),
   clearAlbumFilters: () => set({ albumFilters: { ...EMPTY_FILTERS } }),
